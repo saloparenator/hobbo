d93056a309de10267fb42c86417e9eb59ee05929
saloparenator <saloparenator@gmail.com>
2015-01-07 17:11:57 +0000

    ready to ajax

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
 
+var db = {};
+
 var port = process.env.PORT || 8080;
 //var url = process.env.IP || '0.0.0.0';
+
+console.log("B==D")
+
+var express = require("express");
+
+var app = express();
+
+app.use(function (req, res, next) {
+    var data='';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) { 
+       data += chunk;
+    });
+
+    req.on('end', function() {
+        req.body = data;
+        next();
+    });
+});
+
+app.get('/*', function(req, res, next){
+  console.log(db);
+  if (req.url in db){
+    res.end(db[req.url]);
+  }
+  else
+    res.end();
+});
+
+app.post('/*', function(req, res, next){
+  console.log(req.body);
+  db[req.url] = req.body
+  res.end('ok');
+});
+
+app.listen(port);;
+console.log("http server listening on %d", port);
+
 
 /*
 console.log("B==D")
@@ -46,6 +86,7 @@ wss.on('connection', function connection(ws) {
 });
 */
 
+/*
 var WebSocketServer = require('ws').Server
   , http = require('http')
   , express = require('express')
@@ -56,10 +97,10 @@ app.use(express.static(__dirname + '/public'));
 var server = http.createServer(app);
 server.listen(8080);
 
-var wss = new WebSocketServer({server: server});
+var wss = new WebSocketServer({server: server, verifyClient :function(info,cb){return true;}});
 wss.on('connection', function(ws) {
   var id = setInterval(function() {
-    ws.send(JSON.stringify(process.memoryUsage()), function() { /* ignore errors */ });
+    ws.send(JSON.stringify(process.memoryUsage()), function() { console.log("shit"); });
   }, 100);
   console.log('started client interval');
   ws.on('close', function() {
@@ -67,3 +108,4 @@ wss.on('connection', function(ws) {
     clearInterval(id);
   });
 });
+*/
